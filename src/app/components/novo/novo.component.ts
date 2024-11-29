b7ce58e031ab8057d0cd6d0420ffc3b3193b170d
Nascimento <nascimentosgn@fab.mil.br>
2024-11-29 08:24:29 -0300

    teste

---
+++ src/app/components/novo/novo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-novo',
+  templateUrl: './novo.component.html',
+  styleUrl: './novo.component.scss'
+})
+export class NovoComponent {
+
+}
